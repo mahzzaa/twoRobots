da27635e2e0fba186a2065e783922f57f86e8113
mah <mahsa.79mirmiran@gmail.com>
2023-07-16 10:38:57 +0300

    nav issue fixed

--- script.js
+++ script.js
@@ -1,4 +1,26 @@
-document.addEventListener('DOMContentLoaded',()=>{
-  
+document.addEventListener('DOMContentLoaded', () => {
+    let Nav = document.querySelector('nav');
+    let open = document.querySelector('.open');
+    let close = document.querySelector('.close');
+    let navlink = document.querySelector('.nav-link');
 
+
+
+
+    window.addEventListener('scroll', () => {
+        Nav.classList.toggle('nav-bg', window.scrollY > 0);
+    });
+
+    open.addEventListener('click', ()=>{
+        open.style.display = 'none';
+        close.style.display = 'inline-block';
+        navlink.style.left = '0';
+    });
+
+
+    close.addEventListener('click', ()=>{
+        open.style.display = 'inline-block';
+        close.style.display = 'none';
+        navlink.style.left = '-100%';
+    });
 });
